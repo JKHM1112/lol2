30bab5e0bfbd6a277c402f4cf42b53661f8fc825
이한호 <jk007125@naver.com>
2024-05-27 15:42:44 +0900

    전적검색추가6

--- app/games/[summoner]/components/personalAnalysis.tsx
+++ app/games/[summoner]/components/personalAnalysis.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import { championFull } from "@/app/data/championFull";
+
 type SkillCounts = {
     [key: string]: number;
     Q: number;
     W: number;
     E: number;
 };
+
 export default function PersonalAnalysis({ rankResultTimeline, puuid, championName }: any) {
+
     const characterNumber = rankResultTimeline.info.participants.find((p: any) => p.puuid === puuid)?.participantId
-    function getEventsByParticipantId2(timeline: any, participantId: any) {
+
+    function getEventsByParticipantId(timeline: any, participantId: any) {
         let allEvents: any = [];
         for (const frame of timeline.info.frames) {
             const events = frame.events || [];
@@ -15,23 +20,32 @@ export default function PersonalAnalysis({ rankResultTimeline, puuid, championNa
         }
         return allEvents;
     }
-    const particiapntsTimeline = getEventsByParticipantId2(rankResultTimeline, characterNumber);
-    const skillQIMG = `${championName}Q`;
-    const skillWIMG = `${championName}W`;
-    const skillEIMG = `${championName}E`;
-    const skillRIMG = `${championName}R`;
-    const skillQIMG2 = `Q`;
-    const skillWIMG2 = `W`;
-    const skillEIMG2 = `E`;
-    const skillRIMG2 = `R`;
+    const particiapntsTimeline = getEventsByParticipantId(rankResultTimeline, characterNumber);
+
+    const getSkillImage = (skillSlot: number) => {
+        const champion = Object.values(championFull.data).find((cham: any) => cham.id === championName || cham.key === championName);
+        if (!champion) {
+            return '';
+        }
+        const spell = champion.spells[skillSlot];
+        if (!spell) {
+            return '';
+        }
+        return spell.image.full;
+    };
+
+    const skillQIMG = getSkillImage(0);
+    const skillWIMG = getSkillImage(1);
+    const skillEIMG = getSkillImage(2);
+    const skillRIMG = getSkillImage(3);
 
     const getItemImg = (itemCode: number) => <Image className='rounded-md' alt={'item'} src={`/itemN/${itemCode}.png`} width={30} height={30} />
-    const getSkillImg1 = (skillCode: string) => <Image className='rounded-md' alt={'spell'} src={`/spellE/${skillCode}.png`} width={25} height={25} />
+    const getSkillImg1 = (skillCode: string) => <Image className='rounded-md' alt={'spell'} src={`/spell/${skillCode}`} width={25} height={25} />
     const getSkillImg2 = (skillCode: string) => <Image className='rounded-md' alt={'spell'} src={`/${skillCode}.png`} width={30} height={30} />
-    const itemEvents = particiapntsTimeline.filter((event: any) => event.type === 'ITEM_PURCHASED')
-        .map((event: any) => ({ itemId: event.itemId, timestamp: event.timestamp }));
-    const skillEvents = particiapntsTimeline.filter((event: any) => event.type === 'SKILL_LEVEL_UP')
-        .map((event: any) => ({ skillSlot: event.skillSlot, timestamp: event.timestamp }));
+
+    const itemEvents = particiapntsTimeline.filter((event: any) => event.type === 'ITEM_PURCHASED').map((event: any) => ({ itemId: event.itemId, timestamp: event.timestamp }));
+    const skillEvents = particiapntsTimeline.filter((event: any) => event.type === 'SKILL_LEVEL_UP').map((event: any) => ({ skillSlot: event.skillSlot, timestamp: event.timestamp }));
+
     const itemOrderId = itemEvents.map((event: any) => ({ itemId: event.itemId }))
     const skillOrder = skillEvents.map((event: any) => {
         switch (event.skillSlot) {
@@ -42,18 +56,9 @@ export default function PersonalAnalysis({ rankResultTimeline, puuid, championNa
             default: return '';
         }
     }).filter((skill: string) => skill !== '');
-    console.log(itemOrderId)
-    const skillCounts: SkillCounts = {
-        Q: 0,
-        W: 0,
-        E: 0
-    };
 
-    const skillReachedFive: SkillCounts = {
-        Q: -1,
-        W: -1,
-        E: -1
-    };
+    const skillCounts: SkillCounts = { Q: 0, W: 0, E: 0 };
+    const skillReachedFive: SkillCounts = { Q: -1, W: -1, E: -1 };
 
     skillOrder.forEach((skill: string, index: number) => {
         if (skill === 'Q' || skill === 'W' || skill === 'E') {
@@ -63,6 +68,7 @@ export default function PersonalAnalysis({ rankResultTimeline, puuid, championNa
             }
         }
     });
+
     const sortedSkills = ['Q', 'W', 'E'].sort((a, b) => {
         if (skillCounts[a] !== skillCounts[b]) {
             return skillCounts[b] - skillCounts[a];
@@ -72,13 +78,13 @@ export default function PersonalAnalysis({ rankResultTimeline, puuid, championNa
     });
 
     return (
-        <div className="flex flex-col items-center w-[700px] h-[400px] p-4 box-border border-2 m-0">
+        <div className="flex flex-col items-center w-[700px] min-h-[400px] max-h-[600px] p-4 box-border border-2 m-0">
             {/* 첫 번째 큰 박스 */}
             <div className="flex flex-col h-1/2 w-full border-b-2">
                 <div className="flex border-b-2 p-2 h-[40px]">
                     <div>아이템 빌드</div>
                 </div>
-                <div className="flex flex-wrap p-2 h-[150px]">
+                <div className="flex flex-wrap p-2 min-h-[150px] max-h-[250px]">
                     {itemOrderId.map((item: any, index: number) => (
                         <span key={index} className="w-[50px] h-[50px]">
                             {getItemImg(item.itemId)}
@@ -105,7 +111,7 @@ export default function PersonalAnalysis({ rankResultTimeline, puuid, championNa
                     <div className="flex p-2 h-[150px]">
                         {skillOrder.map((skill: string, index: number) => (
                             <span key={index} className="w-[35px] h-[35px] ">
-                                {getSkillImg2(skill === 'Q' ? skillQIMG2 : skill === 'W' ? skillWIMG2 : skill === 'E' ? skillEIMG2 : skillRIMG2)}
+                                {getSkillImg2(skill === 'Q' ? 'Q' : skill === 'W' ? 'W' : skill === 'E' ? 'E' : 'R')}
                             </span>
                         ))}
                     </div>
