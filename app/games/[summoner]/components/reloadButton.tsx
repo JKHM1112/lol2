7d8650247abf082e89362b5aad477ab09d4485de
이한호 <jk007125@naver.com>
2024-06-02 17:49:01 +0900

    rankgame수정2

--- app/games/[summoner]/components/reloadButton.tsx
+++ app/games/[summoner]/components/reloadButton.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function ReloadButton() {
+    const router = useRouter();
+
     return (
         <div>
-            <Button onClick={() => window.location.reload()}>전적 갱신1</Button>
+            <Button onClick={() => router.refresh()}>전적 갱신</Button>
         </div>
-    )
+    );
 }
